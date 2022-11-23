336a70bb6c600dcca7c74f261ef9161353606603
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-11-23 18:47:16 -0300

    style: UI design update

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const filterElement = document.querySelector('header input')
+
+const cards = document.querySelectorAll('.content li')
+
+filterElement.addEventListener('input', filterCards)
+
+function filterCards() {
+}
