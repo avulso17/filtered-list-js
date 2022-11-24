1191e5259405dc5a03452bc0d550fa5ce8b3fed8
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-11-24 09:15:16 -0300

    feat: filterCards function update

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,20 @@ const cards = document.querySelectorAll('.content li')
 filterElement.addEventListener('input', filterCards)
 
 function filterCards() {
+    if (filterElement.value != '') {
+        for (let card of cards) {
+            const title = card.querySelector('h2').textContent.toLowerCase()
+            const filterText = filterElement.value.toLowerCase()
+
+            if (!title.includes(filterText)) {
+                card.style.display = 'none'
+            } else {
+                card.style.display = 'block'
+            }
+        }
+    } else {
+        for (let card of cards) {
+            card.style.display = 'block'
+        }
+    }
 }
